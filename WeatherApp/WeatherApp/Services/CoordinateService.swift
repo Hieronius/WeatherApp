524ec13d63fabbc55a320f333129282837088947
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-21 03:45:48 +0300

    Add Coordinate service

---
+++ WeatherApp/WeatherApp/Services/CoordinateService.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct CoordinateService {
+    static func isValid(latitude: Double, longitude: Double) -> Bool {
+        // Add validation logic here (e.g., check latitude and longitude ranges)
+        return true
+    }
+}
