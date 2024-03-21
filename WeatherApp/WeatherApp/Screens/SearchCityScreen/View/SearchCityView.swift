e2b405c656d352c414777047e2e29d87ef752de9
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-21 05:03:48 +0300

    add .

--- WeatherApp/WeatherApp/Screens/SearchCityScreen/View/SearchCityView.swift
+++ WeatherApp/WeatherApp/Screens/SearchCityScreen/View/SearchCityView.swift
@@ -2,4 +2,15 @@ import UIKit
 
 final class SearchCityView: UIView {
     
+    // MARK: - Initialization
+    
+    init() {
+        super.init(frame: .zero)
+        backgroundColor = .green
+        // setupViews()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
