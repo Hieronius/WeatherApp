fc24a7e8d3eeec53812f25964ec3613d80005bfc
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-21 06:20:51 +0300

    Update JSONParserService

--- WeatherApp/WeatherApp/Services/JSONParserService.swift
+++ WeatherApp/WeatherApp/Services/JSONParserService.swift
@@ -7,9 +7,9 @@ struct JSONParserService {
         let decoder = JSONDecoder()
         
         do {
-            let weatherData = try decoder.decode(WeatherData.self, from: data)
+            let weatherRawData = try decoder.decode(WeatherRawData.self, from: data)
             // Ensure that the decoding operation succeeded and return the Weather object
-            return Weather(weatherData: weatherData)
+            return Weather(weatherCookedData: weatherRawData)
         } catch {
             // Handle any errors that occur during JSON decoding
             print("Error decoding JSON:", error.localizedDescription)
