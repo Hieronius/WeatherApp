aac6bc68228e8c1c5f5b21671079295f0ee272b0
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-21 05:57:39 +0300

    Edit WeatherFetcher

--- WeatherApp/WeatherApp/Services/WeatherFetcherService.swift
+++ WeatherApp/WeatherApp/Services/WeatherFetcherService.swift
@@ -1,27 +1,52 @@
 import Foundation
 
 struct WeatherFetcherService {
-    
-    func fetchWeather(latitude: Double, longitude: Double, completionHandler: @escaping(Weather) -> Void) {
+    static func fetchWeather(latitude: Double, longitude: Double, completionHandler: @escaping (Result<Weather, Error>) -> Void) {
         
-        let urlString = "https://api.weather.yandex.ru/v2/forecast?lat=\(latitude)&lon=\(longitude)"
+        // Generate the endpoint URL
+        let urlResult = APIEndpointService.generateEndpointURL(latitude: latitude, longitude: longitude, exclude: "minutely,hourly")
         
-        guard let url = URL(string: urlString) else { return }
-        
-        var request = URLRequest(url: url, timeoutInterval: Double.infinity)
-        request.addValue("\(APIKeyService.APIKey)", forHTTPHeaderField: "X-Yandex-API-Key")
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
-            guard let data = data else {
-                print(String(describing: error))
-                return
+        // Check if the result is a success
+        if case let .success(url) = urlResult {
+            // Create a URLRequest with the generated URL
+            var request = URLRequest(url: url, timeoutInterval: Double.infinity)
+            
+            // Set the HTTP method to "GET"
+            request.httpMethod = "GET"
+            
+            // Perform the network request
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                
+                if let error = error {
+                    // Handle the network error
+                    completionHandler(.failure(error))
+                    return
+                }
+                
+                guard let data = data else {
+                    // Handle the case where data is nil
+                    let error = NSError(domain: "DataError", code: 400, userInfo: [NSLocalizedDescriptionKey: "No data received"])
+                    completionHandler(.failure(error))
+                    return
+                }
+                
+                // Parse the JSON data into Weather object
+                if let weather =  JSONParserService.parseJSON(withData: data) {
+                    // Call the completion handler with the weather data
+                    completionHandler(.success(weather))
+                } else {
+                    // Handle the case where parsing JSON fails
+                    let error = NSError(domain: "JSONParsingError", code: 400, userInfo: [NSLocalizedDescriptionKey: "Failed to parse JSON"])
+                    completionHandler(.failure(error))
+                }
             }
-            if let weather = JSONParserService.parseJSON(withData: data) {
-                completionHandler(weather)
-            }
+            
+            // Resume the task
+            task.resume()
+            
+        } else if case let .failure(error) = urlResult {
+            // Handle the case where generating the endpoint URL fails
+            completionHandler(.failure(error))
         }
-        
-        task.resume()
     }
 }
