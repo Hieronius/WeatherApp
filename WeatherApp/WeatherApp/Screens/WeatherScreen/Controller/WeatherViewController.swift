8344d1517feacf27c22c680e15e8a41fd1872ef4
Arsenty Halimovsky <ghost12@yandex.ru>
2024-03-22 03:28:53 +0300

    Call WeatherFetcherService

--- WeatherApp/WeatherApp/Screens/WeatherScreen/Controller/WeatherViewController.swift
+++ WeatherApp/WeatherApp/Screens/WeatherScreen/Controller/WeatherViewController.swift
@@ -7,7 +7,18 @@ final class WeatherViewController: GenericViewController<WeatherView> {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        WeatherFetcherService.fetchWeather(latitude: 55.7558, longitude: 37.6173) { result in
+            switch result {
+                case .success(let weatherData):
+                    // Handle the successful retrieval of weather data
+                    // You can use the weatherData object here to update your UI or perform other tasks
+                    print("Weather data fetched successfully:", weatherData)
+                case .failure(let error):
+                    // Handle the failure to fetch weather data
+                    // You can display an error message or perform other error-handling tasks
+                    print("Error fetching weather data:", error.localizedDescription)
+                }
+        }
         print("hello")
     }
 
